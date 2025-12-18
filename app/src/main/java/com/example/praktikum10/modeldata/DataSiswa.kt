109dadae54eb8ecd0f1a44279e939d21ee812421
rnktlhr <rinakitluhur10@gmail.com>
2025-12-18 11:57:34 +0700

    menambahkan data class fun

--- app/src/main/java/com/example/praktikum10/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/praktikum10/modeldata/DataSiswa.kt
@@ -16,3 +16,25 @@ data class UIStateSiswa(
     val isEntryValid: Boolean = false
 )
 
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
