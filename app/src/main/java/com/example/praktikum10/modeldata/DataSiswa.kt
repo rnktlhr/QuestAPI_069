66e67a405f55780f340fb0a0bc425891e14cc775
rnktlhr <rinakitluhur10@gmail.com>
2025-12-18 11:56:17 +0700

    menambahkan data class data siswa

---
+++ app/src/main/java/com/example/praktikum10/modeldata/DataSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.praktikum10.modeldata
+
+import android.provider.ContactsContract
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
+
