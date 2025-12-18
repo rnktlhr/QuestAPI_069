0aa9a62a073c3fefed30a23ce198e2ac244713a9
rnktlhr <rinakitluhur10@gmail.com>
2025-12-18 12:55:48 +0700

    menambahkan object PenyediaViewModel

--- app/src/main/java/com/example/praktikum10/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/provider/PenyediaViewModel.kt
@@ -2,8 +2,23 @@ package com.example.praktikum10.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.praktikum10.repository.AplikasiDataSiswa
+import com.example.praktikum10.viewmodel.EntryViewModel
+import com.example.praktikum10.viewmodel.HomeViewModel
+
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
         )
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiDataSiswa().containerApp.repositoryDataSiswa)
+        }
+    }
+}
