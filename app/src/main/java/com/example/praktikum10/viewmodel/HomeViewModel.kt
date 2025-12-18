f84d31820a5d4fc26386a360255b71a4b39bfa56
rnktlhr <rinakitluhur10@gmail.com>
2025-12-18 12:28:29 +0700

    menambahkan class HomeViewModel

--- app/src/main/java/com/example/praktikum10/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/HomeViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.praktikum10.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.praktikum10.modeldata.DataSiswa
+import com.example.praktikum10.repository.RepositoryDataSiswa
 
 sealed interface StatusUiSiswa {
     data class Success(val listSiswa: List<DataSiswa>) : StatusUiSiswa
@@ -8,3 +13,11 @@ sealed interface StatusUiSiswa {
     object Loading : StatusUiSiswa
 }
 
+
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel(){
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+    init {
+        loadSiswa()
+    }
+}
