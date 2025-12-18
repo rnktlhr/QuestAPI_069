1f4b8ce47cc6c2143be73f45b129d5eb6296eee3
rnktlhr <rinakitluhur10@gmail.com>
2025-12-18 12:38:44 +0700

    menambahkan class EntryViewModel

---
+++ app/src/main/java/com/example/praktikum10/viewmodel/EntryViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.praktikum10.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.modeldata.DetailSiswa
+import com.example.praktikum10.modeldata.UIStateSiswa
+import com.example.praktikum10.repository.RepositoryDataSiswa
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+}
