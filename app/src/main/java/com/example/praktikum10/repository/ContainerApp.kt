975bd4502c1332fbcc32d19d5a0a66b4ae78232f
rnktlhr <rinakitluhur10@gmail.com>
2025-12-18 12:23:28 +0700

    menambahkan class default container app

---
+++ app/src/main/java/com/example/praktikum10/repository/ContainerApp.kt
@@ -0,0 +1,49 @@
+package com.example.praktikum10.repository
+
+import android.app.Application
+import com.example.praktikum10.apiservice.ServiceApiSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa : RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+
+    private val baseurl = "http://10.0.2.2/pam/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klient = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klient)
+        .build()
+
+    private val retrofitService: ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(retrofitService)
+
+    }
+}
+
