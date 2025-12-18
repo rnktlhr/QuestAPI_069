d8fbac24786905986e491d6bb07deef79d590811
rnktlhr <rinakitluhur10@gmail.com>
2025-12-18 12:07:29 +0700

    menambahkan class jaringan repodata siswa

--- app/src/main/java/com/example/praktikum10/repository/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/praktikum10/repository/RepositoryDataSiswa.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum10.repository
 
+import com.example.praktikum10.apiservice.ServiceApiSiswa
 import com.example.praktikum10.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
@@ -7,3 +8,9 @@ interface RepositoryDataSiswa {
     suspend fun postDatasiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa {
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDatasiswa(dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
