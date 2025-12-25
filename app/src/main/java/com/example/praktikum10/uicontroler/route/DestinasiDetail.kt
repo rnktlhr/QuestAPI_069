62b837820e115141bd9202f7d8d6ba01ed815195
rnktlhr <rinakitluhur10@gmail.com>
2025-12-25 17:51:05 +0700

    menambah DestinasiDetail.kt

---
+++ app/src/main/java/com/example/praktikum10/uicontroler/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum10.uicontroler.route
+
+import com.example.praktikum10.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
